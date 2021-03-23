887625da62543407ad438d7eefc10d7d0c680d49
hyebin829 <smilerain779@gmail.com>
2021-03-23 11:06:48 +0900

    Add getApi function

---
+++ src/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const getApi = async () => {
+  const getToken = await axios.post(
+    " https://id.twitch.tv/oauth2/token?client_id=9q6oisptyvvdncbo559zgs7x2n1w2a&client_secret=ztzt4a2v7xie59xbxyjezdkjirl3w9&grant_type=client_credentials"
+  );
+  console.log(getToken.data.access_token);
+  const api = axios
+    .get("https://api.twitch.tv/helix/streams?user_login=hanryang1125", {
+      headers: {
+        "Client-ID": "9q6oisptyvvdncbo559zgs7x2n1w2a",
+        Authorization: `Bearer ${getToken.data.access_token}`,
+      },
+    })
+    .then((res) => console.log(res));
+};
+getApi();
+
+export default getApi;
