7164be50a53e6de96d1b97070d68cff4c124647d
hyebin829 <smilerain779@gmail.com>
2021-03-29 16:34:01 +0900

    Fix Streaminfo function

--- src/streaminfo.js
+++ src/streaminfo.js
@@ -4,27 +4,40 @@ import { useState, useEffect } from "react";
 function Streaminfo() {
   const [streaminfo, setStreaminfo] = useState([]);
 
+  const getApi = async () => {
+    const getToken = await axios.post(
+      " https://id.twitch.tv/oauth2/token?client_id=9q6oisptyvvdncbo559zgs7x2n1w2a&client_secret=ztzt4a2v7xie59xbxyjezdkjirl3w9&grant_type=client_credentials"
+    );
+    // console.log(getToken.data.access_token);
+    const api = axios
+      .get("https://api.twitch.tv/helix/streams?user_login=yapyap30", {
+        headers: {
+          "Client-ID": "9q6oisptyvvdncbo559zgs7x2n1w2a",
+          Authorization: `Bearer ${getToken.data.access_token}`,
+        },
+      })
+      .then((res) => {
+        setStreaminfo([res.data.data[0]]);
+        console.log(res.data.data[0]);
+      });
+  };
+
   useEffect(() => {
-    const getApi = async () => {
-      const getToken = await axios.post(
-        " https://id.twitch.tv/oauth2/token?client_id=9q6oisptyvvdncbo559zgs7x2n1w2a&client_secret=ztzt4a2v7xie59xbxyjezdkjirl3w9&grant_type=client_credentials"
-      );
-      // console.log(getToken.data.access_token);
-      const api = axios
-        .get("https://api.twitch.tv/helix/streams?user_login=hanryang1125", {
-          headers: {
-            "Client-ID": "9q6oisptyvvdncbo559zgs7x2n1w2a",
-            Authorization: `Bearer ${getToken.data.access_token}`,
-          },
-        })
-        .then((res) => {
-          const info = setStreaminfo(res.data.data[0]);
-        });
-    };
     getApi();
   }, [setStreaminfo]);
-  return <div></div>;
+  return (
+    <>
+      {streaminfo.map((data) => (
+        <>
+          {" "}
+          <div>{data.title}</div>
+          <div>{data.game_name}</div>
+          <div>{data.viewer_count}</div>
+        </>
+      ))}
+    </>
+  );
 }
-// setInterval(getApi, 10000);
+// setInterval(Streaminfo, 10000);
 
 export default Streaminfo;
